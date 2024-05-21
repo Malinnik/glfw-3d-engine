fb5fe908afd72428375af8683cb6716c937dd083
Malinnik <ratanov.maxim@gmail.com>
2024-05-21 17:50:13 +0500

    Changed name

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     return -1;
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  window = glfwCreateWindow(640, 480, "3D-Engine", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
