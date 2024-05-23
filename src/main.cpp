a893030246269390d553e7ed737119168a552347
Malinnik <ratanov.maxim@gmail.com>
2024-05-23 13:51:40 +0500

    added: glad render

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 
 #include "gui.h"
 
+#include <iostream>
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+
+// change viewport size after window size change
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
+
 int main(int argc, char *argv[]) {
 
   GLFWwindow *window;
@@ -10,14 +22,20 @@ int main(int argc, char *argv[]) {
   if (!glfwInit())
     return -1;
 
-  window = glfwCreateWindow(640, 480, "3D-Engine", NULL, NULL);
+  window = glfwCreateWindow(WIDTH, HEIGHT, "3D-Engine", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
-
   glfwMakeContextCurrent(window);
 
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+  } 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
 
   imgui gui(window);
@@ -29,6 +47,7 @@ int main(int argc, char *argv[]) {
     gui.draw_frame();
 
     // GLFW render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     // ImGui render
