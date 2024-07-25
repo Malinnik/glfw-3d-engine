61a9e4607c34deaaf33c5d1d1aa37e7d009a57c5
Malinnik <ratanov.maxim@gmail.com>
2024-07-25 20:09:03 +0500

    WIP trying to triangle

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
   if (!glfwInit())
     return -1;
 
-  window = glfwCreateWindow(WIDTH, HEIGHT, "3D-Engine", NULL, NULL);
+  window = glfwCreateWindow(WIDTH, HEIGHT, "3D-Engine", glfwGetPrimaryMonitor(), NULL);
   if (!window) {
     glfwTerminate();
     return -1;
@@ -41,6 +41,44 @@ int main(int argc, char *argv[]) {
   imgui gui(window);
   gui.init();
 
+
+  // Triangle
+
+  GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+  };
+
+  // float vertices[] = {
+  //   0.5f,  0.5f, 0.0f,  // top right
+  //   0.5f, -0.5f, 0.0f,  // bottom right
+  //   -0.5f, -0.5, 0.0f,  // bottom left
+  //   -0.5f,  0.5f, 0.0f   // top left  
+  // };
+
+  unsigned int indices[] = {
+    0,1,3,
+    1,2,3
+  };
+
+  GLuint VAO, VBO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  (VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+
   while (!glfwWindowShouldClose(window)) {
     // ImGui frames
     gui.new_frame();
@@ -49,6 +87,7 @@ int main(int argc, char *argv[]) {
     // GLFW render
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // ImGui render
     gui.render();
@@ -60,6 +99,9 @@ int main(int argc, char *argv[]) {
     glfwPollEvents();
   }
 
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
 
   // gui.cleanup();
   glfwTerminate();
