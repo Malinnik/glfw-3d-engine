4c568bc78532d594421750054970f2767d071774
Malinnik <ratanov.maxim@gmail.com>
2024-07-25 22:17:54 +0500

    WIP Plane in two Triangles

--- src/main.cpp
+++ src/main.cpp
@@ -100,9 +100,10 @@ int main(int argc, char *argv[]) {
   //          Triangle
 
   GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    0.5f,  0.5f, 0.0f,  // Верхний правый угол
+    0.5f, -0.5f, 0.0f,  // Нижний правый угол
+    -0.5f, -0.5f, 0.0f,  // Нижний левый угол
+    -0.5f,  0.5f, 0.0f   // Верхний левый угол
   };
 
   unsigned int indices[] = {
@@ -110,24 +111,32 @@ int main(int argc, char *argv[]) {
     1,2,3
   };
 
-  GLuint VBO, VAO;
+  GLuint VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
   // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
   glEnableVertexAttribArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
 
-  glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+  glBindVertexArray(0); // Unbind VAO (it's always a good thing to   unbind any buffer/array to prevent strange bugs)
 
 
   //------------------------------------------------------------------------------------------------------------------------------
+
+  // Uncommenting this call will result in wireframe polygons.
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   while (!glfwWindowShouldClose(window)) {
     // ImGui frames
@@ -144,7 +153,7 @@ int main(int argc, char *argv[]) {
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     // ImGui render
