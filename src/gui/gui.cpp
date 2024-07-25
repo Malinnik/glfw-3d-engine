fbfc6f2967fe63723272161ec4061725c7288d52
Malinnik <ratanov.maxim@gmail.com>
2024-07-25 20:04:59 +0500

    More ImGui tests

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -3,9 +3,13 @@
 #include "imgui_impl_opengl3.h"
 #include <imgui.h>
 
+#include <string>
+
 imgui::imgui(GLFWwindow *window) { this->window = window; }
 
 imgui::~imgui() { this->cleanup(); }
+
+
 
 void imgui::init() {
   IMGUI_CHECKVERSION();
@@ -23,17 +27,6 @@ void imgui::new_frame() {
   ImGui::NewFrame();
 }
 
-void imgui::draw_frame() {
-  ImGui::SetWindowPos(ImVec2(0, 0));
-  ImGui::SetWindowSize(ImVec2(400, 400));
-
-  ImGui::Begin("Demo window");
-
-  ImGui::Button("Hello!");
-
-  ImGui::End();
-}
-
 void imgui::render() {
   ImGui::Render();
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -43,4 +36,34 @@ void imgui::cleanup() {
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
+}
+
+
+float red = 0.1f;
+float green = 0.0f;
+ImVec2 button_size = ImVec2(100, 100);
+
+void imgui::draw_frame() {
+  ImGui::SetWindowPos(ImVec2(0, 0));
+  ImGui::SetWindowSize(ImVec2(400, 400));
+
+  ImGui::Begin("Demo window");
+
+    ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
+
+    ImGui::ColorEdit4("Test Color", &red);
+    ImGui::Text("RED: %f", red);
+
+    if (ImGui::Button("Add"))
+      green += 0.1f;
+    ImGui::SliderFloat("Test Slider", &green, 0.0f, 1.0f);
+    ImGui::Text("Green: %f", green);
+
+    ImGui::Text("Time: %f", ImGui::GetTime());
+    
+
+    if (ImGui::Button("Exit"))
+      exit(0);
+
+  ImGui::End();
 }
