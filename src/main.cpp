355df21c0f361a99ecd4c169cb904f4f299dfe1e
Malinnik <ratanov.maxim@gmail.com>
2024-08-08 01:29:28 +0500

    fix(main): another code indent

--- src/main.cpp
+++ src/main.cpp
@@ -127,16 +127,13 @@ int main(int argc, char *argv[]) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          (GLvoid *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(
-        GL_ARRAY_BUFFER,
-        0); // Note that this is allowed, the call to glVertexAttribPointer
+    glBindBuffer(GL_ARRAY_BUFFER,0); 
+            // Note that this is allowed, the call to glVertexAttribPointer
             // registered VBO as the currently bound vertex buffer object so
             // afterwards we can safely unbind
 
