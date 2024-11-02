2077d976292493fd2b5d160ae0a87f3774764aa2
Malinnik <ratanov.maxim@gmail.com>
2024-11-02 16:49:35 +0500

    feat(debug): added saving text to file

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1,9 +1,12 @@
 #include "gui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include <X11/Xlib.h>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
 #include <imgui.h>
-
-#include <string>
+#include <iterator>
 
 imgui::imgui(GLFWwindow *window) { this->window = window; }
 
@@ -65,30 +68,28 @@ void imgui::draw_frame() {
 }
 
 void imgui::show_debug_shaders(const char *vshader, const char *fshader) {
-    ImGui::Text("Vertex:");
-    ImGui::Separator();
-    ImGui::Text(vshader);
+  ImGui::Text("Vertex:");
+  ImGui::Separator();
+  ImGui::Text("%s", vshader);
 
-    for (int i = 0; i < 10; i++)
-      ImGui::Spacing();
+  for (int i = 0; i < 10; i++)
+    ImGui::Spacing();
 
-    ImGui::Text("Fragment:");
-    ImGui::Separator();
-    ImGui::Text(fshader);
+  ImGui::Text("Fragment:");
+  ImGui::Separator();
+  ImGui::Text("%s", fshader);
 }
 
 void imgui::main_bar(const char *vshader, const char *fshader) {
   ImGui::BeginMainMenuBar();
   if (ImGui::BeginMenu("File", true)) {
     if (ImGui::MenuItem("Open")) {
-
     }
     if (ImGui::MenuItem("Save")) {
-
     }
     ImGui::EndMenu();
   }
-  
+
   if (ImGui::BeginMenu("Debug", true)) {
     if (ImGui::Button("Show shader")) {
       ImGui::OpenPopup("Debug shader");
@@ -103,31 +104,68 @@ void imgui::main_bar(const char *vshader, const char *fshader) {
     ImGui::Bullet();
     ImGui::Text("FPS: %f", ImGui::GetIO().Framerate);
 
-    if (ImGui::BeginPopup("Debug shader")){
+    if (ImGui::BeginPopup("Debug shader")) {
       show_debug_shaders(vshader, fshader);
       ImGui::EndPopup();
     }
-    if (ImGui::BeginPopup("Debug metrics"))
-    {
+    if (ImGui::BeginPopup("Debug metrics")) {
       ImGui::ShowMetricsWindow();
       ImGui::EndPopup();
     }
-    if (ImGui::BeginPopup("Test File Save")){
-      // char *text = "Some text";
-      
-      // ImGui::InputTextMultiline("Text Input", text, 2*1024);
+    if (ImGui::BeginPopup("Test File Save")) {
+      struct Funcs {
+        static int MyResizeCallback(ImGuiInputTextCallbackData *data) {
+          if (data->EventFlag == ImGuiInputTextFlags_CallbackResize) {
+            ImVector<char> *my_str = (ImVector<char> *)data->UserData;
+            IM_ASSERT(my_str->begin() == data->Buf);
+            my_str->resize(
+                data->BufSize); // NB: On resizing calls, generally
+                                // data->BufSize == data->BufTextLen + 1
+            data->Buf = my_str->begin();
+          }
+          return 0;
+        }
+        static bool MyInputTextMultiline(const char *label,
+                                         ImVector<char> *my_str,
+                                         const ImVec2 &size = ImVec2(0, 0),
+                                         ImGuiInputTextFlags flags = 0) {
+          IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+          return ImGui::InputTextMultiline(
+              label, my_str->begin(), (size_t)my_str->size(), size,
+              flags | ImGuiInputTextFlags_CallbackResize,
+              Funcs::MyResizeCallback, (void *)my_str);
+        }
+
+        static void SaveTextToFile(ImVector<char> *data) {
+          std::ofstream output_file("./example.txt");
+          std::ostream_iterator<char> output_iterator(output_file);
+          std::copy(std::begin(*data), std::end(*data), output_iterator);
+        }
+      };
+
+      static ImVector<char> str;
+      if (str.empty())
+        str.push_back(0);
+      Funcs::MyInputTextMultiline(
+          "##Save text to File", &str,
+          ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16));
+      ImGui::Text("Data: %p\nSize: %d\nCapacity: %d", (void *)str.begin(),
+                  str.size(), str.capacity());
+      if (ImGui::Button("Save"))
+        Funcs::SaveTextToFile(&str);
       ImGui::EndPopup();
     }
 
-    
     ImGui::EndMenu();
   }
 
   // Move button to the right
-  ImGuiStyle& style = ImGui::GetStyle();
-  float buttonWidth1 = ImGui::CalcTextSize("Exit").x + style.FramePadding.x * 2.f;
+  ImGuiStyle &style = ImGui::GetStyle();
+  float buttonWidth1 =
+      ImGui::CalcTextSize("Exit").x + style.FramePadding.x * 2.f;
   float widthNeeded = buttonWidth1 + style.ItemSpacing.x;
-  ImGui::SetCursorPosX(ImGui::GetCursorPosX() + ImGui::GetContentRegionAvail().x - widthNeeded);
+  ImGui::SetCursorPosX(ImGui::GetCursorPosX() +
+                       ImGui::GetContentRegionAvail().x - widthNeeded);
   if (ImGui::Button("Exit"))
     exit(0);
 
--- src/include/gui.h
+++ src/include/gui.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <GLFW/glfw3.h>
-#include <string>
 
 class imgui {
 private:
