c8852a105b7cdc16b0f59cc71dead14fc54e6573
Malinnik <ratanov.maxim@gmail.com>
2025-12-04 19:02:47 +0500

    refactor: deleted logging from inputLoop

--- src/engine/InputLoop.cpp
+++ src/engine/InputLoop.cpp
@@ -16,34 +16,27 @@ void InputLoop::inputLoop(){
     updateTime();
 
     if (Events::jPressed(GLFW_KEY_TAB)){
-        LOG_F(INFO, "Key TAB just pressed");
         Events::toggleCursor();
     }
 
     if (Events::pressed(GLFW_KEY_W)){
-        LOG_F(INFO, "Key W pressed");
         camera->position += camera->front * delta * camera->speed;
     }
     if (Events::pressed(GLFW_KEY_S)){
-        LOG_F(INFO, "Key S pressed");
         camera->position -= camera->front * delta * camera->speed;
     }
     if (Events::pressed(GLFW_KEY_D)){
-        LOG_F(INFO, "Key D pressed");
         camera->position += camera->right * delta * camera->speed;
     }
     if (Events::pressed(GLFW_KEY_A)){
-        LOG_F(INFO, "Key A pressed");
         camera->position -= camera->right * delta * camera->speed;
     }
 
     if (Events::pressed(GLFW_KEY_SPACE)){
-        LOG_F(INFO, "Key SPACE pressed");
         camera->position += camera->up * delta * camera->speed;
     }
 
     if (Events::pressed(GLFW_KEY_LEFT_SHIFT)){
-        LOG_F(INFO, "Key LEFT SHIFT pressed");
         camera->position -= camera->up * delta * camera->speed;
     }
 
