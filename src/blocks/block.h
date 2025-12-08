4ba220e4edf024c8c06f64d69bdb94f8b9088a71
Malinnik <ratanov.maxim@gmail.com>
2025-12-08 17:54:50 +0500

    WIP namespace blocks

--- src/blocks/block.h
+++ src/blocks/block.h
@@ -2,6 +2,25 @@
 
 #include <cstdint>
 
+
+namespace blocks
+{
+    class Block
+    {
+        uint8_t id;
+            
+    };
+
+    class BlockRegistries
+    {
+        static uint8_t registerBlock(Block block);
+
+    };
+
+    
+
+}
+
 struct block {
     uint8_t id;
 };
