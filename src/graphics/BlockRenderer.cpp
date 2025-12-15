4ef8dd9b73275be74de70f740a50c05293bd81dc
Malinnik <ratanov.maxim@gmail.com>
2025-12-15 11:03:33 +0500

    fix: returned defines back

--- src/graphics/BlockRenderer.cpp
+++ src/graphics/BlockRenderer.cpp
@@ -1,23 +1,37 @@
 #include "BlockRenderer.h"
-#include "engine/resources/ResourceLocation.h"
+#include "graphics/mesh.h"
+#include "blocks/block.h"
 
-// vert coords and texture coords
-#define vertex_SIZE (3+2+1)
+#define VERTEX_SIZE (3 + 2 + 1)
 
-int chunk_attrs[] = {3,2,1,0};
+#define CDIV(X,A) (((X) < 0) ? ((X) / (A) - 1) : ((X) / (A)))
+#define LOCAL_NEG(X, SIZE) (((X) < 0) ? ((SIZE)+(X)) : (X))
+#define LOCAL(X, SIZE) ((X) >= (SIZE) ? ((X) - (SIZE)) : LOCAL_NEG(X, SIZE))
+#define IS_CHUNK(X,Y,Z) (GET_CHUNK(X,Y,Z) != nullptr)
+#define GET_CHUNK(X,Y,Z) (chunks[((CDIV(Y, CHUNK_H)+1) * 3 + CDIV(Z, CHUNK_D) + 1) * 3 + CDIV(X, CHUNK_W) + 1])
 
-BlockRenderer::BlockRenderer(size_t capacity) : capacity(capacity)
-{
-    buffer = new float[capacity * vertex_SIZE * 6];
+#define BLOCK(X,Y,Z) (blocks::get(GET_CHUNK(X,Y,Z)->blocksIds[(LOCAL(Y, CHUNK_H) * CHUNK_D + LOCAL(Z, CHUNK_D)) * CHUNK_W + LOCAL(X, CHUNK_W)]))
+#define IS_BLOCKED(X,Y,Z) ((!IS_CHUNK(X, Y, Z)) || BLOCK(X, Y, Z)->id)
 
+#define VERTEX(INDEX, X,Y,Z, U,V, L) buffer[INDEX+0] = (X);\
+								  buffer[INDEX+1] = (Y);\
+								  buffer[INDEX+2] = (Z);\
+								  buffer[INDEX+3] = (U);\
+								  buffer[INDEX+4] = (V);\
+								  buffer[INDEX+5] = (L);\
+								  INDEX += VERTEX_SIZE;
+
+int chunk_attrs[] = {3,2,1, 0};
+
+BlockRenderer::BlockRenderer(size_t capacity) : capacity(capacity) {
+	buffer = new float[capacity * VERTEX_SIZE * 6];
 }
 
-BlockRenderer::~BlockRenderer()
-{
-    delete[] buffer;
+BlockRenderer::~BlockRenderer(){
+	delete[] buffer;
 }
 
-std::tuple<int, int, int, int> BlockRenderer::calculateUV(resources::ResourceLocation* location)
+std::tuple<float, float, float, float> BlockRenderer::calculateUV(resources::ResourceLocation *location)
 {
 	unsigned int id = location->getID();
 	float uvsize = 1.0f/16.0f;
@@ -25,241 +39,181 @@ std::tuple<int, int, int, int> BlockRenderer::calculateUV(resources::ResourceLoc
 	float v1 = 1-((1 + id / 16) * uvsize);
 	float u2 = u1 + uvsize;
 	float v2 = v1 + uvsize;
-
-	return {u1, v1, u2, v2};
+    return std::tuple<float, float, float, float>(u1, v1, u2, v2);
 }
 
-
-Mesh *BlockRenderer::render(Chunk *chunk, const Chunk** chunks, bool ambientOcclusion)
-{
+Mesh* BlockRenderer::render(Chunk* chunk, const Chunk** chunks, bool ambientOcclusion){
 	float aoFactor = 0.15f;
-    size_t index = 0;
+	size_t index = 0;
 	for (int y = 0; y < CHUNK_H; y++){
 		for (int z = 0; z < CHUNK_D; z++){
 			for (int x = 0; x < CHUNK_W; x++){
 				unsigned int id = chunk->blocksIds[(y * CHUNK_D + z) * CHUNK_W + x];
-				blocks::Block* block = blocks::get(id);
-				// unsigned int id = blk.id;
-				if (id == 0){
+				if (!id){
 					continue;
 				}
+
+				blocks::Block* block = blocks::get(id);
+
 				float l;
+				float uvsize = 1.0f/16.0f;
+				float u1 = (id % 16) * uvsize;
+				float v1 = 1-((1 + id / 16) * uvsize);
+				float u2 = u1 + uvsize;
+				float v2 = v1 + uvsize;
+
+				// AO values
 				float a,b,c,d,e,f,g,h;
 				a = b = c = d = e = f = g = h = 0.0f;
-				
-                if (!isBlocked(x,y+1,z)){
+
+				if (!IS_BLOCKED(x,y+1,z)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.up);
 					l = 1.0f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.up);
 
 					if (ambientOcclusion){
-						a = isBlocked(x+1,y+1,z)*aoFactor;
-						b = isBlocked(x,y+1,z+1)*aoFactor;
-						c = isBlocked(x-1,y+1,z)*aoFactor;
-						d = isBlocked(x,y+1,z-1)*aoFactor;
+						a = IS_BLOCKED(x+1,y+1,z)*aoFactor;
+						b = IS_BLOCKED(x,y+1,z+1)*aoFactor;
+						c = IS_BLOCKED(x-1,y+1,z)*aoFactor;
+						d = IS_BLOCKED(x,y+1,z-1)*aoFactor;
 
-						e = isBlocked(x-1,y+1,z-1)*aoFactor;
-						f = isBlocked(x-1,y+1,z+1)*aoFactor;
-						g = isBlocked(x+1,y+1,z+1)*aoFactor;
-						h = isBlocked(x+1,y+1,z-1)*aoFactor;
+						e = IS_BLOCKED(x-1,y+1,z-1)*aoFactor;
+						f = IS_BLOCKED(x-1,y+1,z+1)*aoFactor;
+						g = IS_BLOCKED(x+1,y+1,z+1)*aoFactor;
+						h = IS_BLOCKED(x+1,y+1,z-1)*aoFactor;
 					}
 
-					vertex(&index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-c-b-f));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-c-b-f));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
 
-					vertex(&index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
-					vertex(&index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v1, l*(1.0f-a-d-h));
+					VERTEX(index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v1, l*(1.0f-a-d-h));
 				}
-				if (!isBlocked(x,y-1,z)){
+				if (!IS_BLOCKED(x,y-1,z)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.down);
 					l = 0.75f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.down);
 
 					if (ambientOcclusion){
-						a = isBlocked(x+1,y-1,z)*aoFactor;
-						b = isBlocked(x,y-1,z+1)*aoFactor;
-						c = isBlocked(x-1,y-1,z)*aoFactor;
-						d = isBlocked(x,y-1,z-1)*aoFactor;
+						a = IS_BLOCKED(x+1,y-1,z)*aoFactor;
+						b = IS_BLOCKED(x,y-1,z+1)*aoFactor;
+						c = IS_BLOCKED(x-1,y-1,z)*aoFactor;
+						d = IS_BLOCKED(x,y-1,z-1)*aoFactor;
 
-						e = isBlocked(x-1,y-1,z-1)*aoFactor;
-						f = isBlocked(x-1,y-1,z+1)*aoFactor;
-						g = isBlocked(x+1,y-1,z+1)*aoFactor;
-						h = isBlocked(x+1,y-1,z-1)*aoFactor;
+						e = IS_BLOCKED(x-1,y-1,z-1)*aoFactor;
+						f = IS_BLOCKED(x-1,y-1,z+1)*aoFactor;
+						g = IS_BLOCKED(x+1,y-1,z+1)*aoFactor;
+						h = IS_BLOCKED(x+1,y-1,z-1)*aoFactor;
 					}
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
-					vertex(&index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v2, l*(1.0f-c-b-f));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v2, l*(1.0f-c-b-f));
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-a-d-h));
-					vertex(&index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-a-d-h));
+					VERTEX(index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
 				}
 
-				if (!isBlocked(x+1,y,z)){
+				if (!IS_BLOCKED(x+1,y,z)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.right);
 					l = 0.95f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.right);
 
 					if (ambientOcclusion){
-						a = isBlocked(x+1,y+1,z)*aoFactor;
-						b = isBlocked(x+1,y,  z+1)*aoFactor;
-						c = isBlocked(x+1,y-1,z)*aoFactor;
-						d = isBlocked(x+1,y,  z-1)*aoFactor;
+						a = IS_BLOCKED(x+1,y+1,z)*aoFactor;
+						b = IS_BLOCKED(x+1,y,  z+1)*aoFactor;
+						c = IS_BLOCKED(x+1,y-1,z)*aoFactor;
+						d = IS_BLOCKED(x+1,y,  z-1)*aoFactor;
 
-						e = isBlocked(x+1,y-1,z-1)*aoFactor;
-						f = isBlocked(x+1,y-1,z+1)*aoFactor;
-						g = isBlocked(x+1,y+1,z+1)*aoFactor;
-						h = isBlocked(x+1,y+1,z-1)*aoFactor;
+						e = IS_BLOCKED(x+1,y-1,z-1)*aoFactor;
+						f = IS_BLOCKED(x+1,y-1,z+1)*aoFactor;
+						g = IS_BLOCKED(x+1,y+1,z+1)*aoFactor;
+						h = IS_BLOCKED(x+1,y+1,z-1)*aoFactor;
 					}
 
-					vertex(&index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y + 0.5f, z - 0.5f, u2,v2, l*(1.0f-d-a-h));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y + 0.5f, z - 0.5f, u2,v2, l*(1.0f-d-a-h));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
 
-					vertex(&index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
-					vertex(&index, x + 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-b-c-f));
+					VERTEX(index, x + 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x + 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-b-c-f));
 				}
-				if (!isBlocked(x-1,y,z)){
+				if (!IS_BLOCKED(x-1,y,z)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.left);
 					l = 0.85f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.left);
 
 					if (ambientOcclusion){
-						a = isBlocked(x-1,y+1,z)*aoFactor;
-						b = isBlocked(x-1,y,  z+1)*aoFactor;
-						c = isBlocked(x-1,y-1,z)*aoFactor;
-						d = isBlocked(x-1,y,  z-1)*aoFactor;
+						a = IS_BLOCKED(x-1,y+1,z)*aoFactor;
+						b = IS_BLOCKED(x-1,y,  z+1)*aoFactor;
+						c = IS_BLOCKED(x-1,y-1,z)*aoFactor;
+						d = IS_BLOCKED(x-1,y,  z-1)*aoFactor;
 
-						e = isBlocked(x-1,y-1,z-1)*aoFactor;
-						f = isBlocked(x-1,y-1,z+1)*aoFactor;
-						g = isBlocked(x-1,y+1,z+1)*aoFactor;
-						h = isBlocked(x-1,y+1,z-1)*aoFactor;
+						e = IS_BLOCKED(x-1,y-1,z-1)*aoFactor;
+						f = IS_BLOCKED(x-1,y-1,z+1)*aoFactor;
+						g = IS_BLOCKED(x-1,y+1,z+1)*aoFactor;
+						h = IS_BLOCKED(x-1,y+1,z-1)*aoFactor;
 					}
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
-					vertex(&index, x - 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-d-a-h));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-d-a-h));
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x - 0.5f, y - 0.5f, z + 0.5f, u2,v1, l*(1.0f-b-c-f));
-					vertex(&index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x - 0.5f, y - 0.5f, z + 0.5f, u2,v1, l*(1.0f-b-c-f));
+					VERTEX(index, x - 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
 				}
 
-				if (!isBlocked(x,y,z+1)){
+				if (!IS_BLOCKED(x,y,z+1)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.front);
 					l = 0.9f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.front);
 
 					if (ambientOcclusion){
-						a = isBlocked(x,  y+1,z+1)*aoFactor;
-						b = isBlocked(x+1,y,  z+1)*aoFactor;
-						c = isBlocked(x,  y-1,z+1)*aoFactor;
-						d = isBlocked(x-1,y,  z+1)*aoFactor;
+						a = IS_BLOCKED(x,  y+1,z+1)*aoFactor;
+						b = IS_BLOCKED(x+1,y,  z+1)*aoFactor;
+						c = IS_BLOCKED(x,  y-1,z+1)*aoFactor;
+						d = IS_BLOCKED(x-1,y,  z+1)*aoFactor;
 
-						e = isBlocked(x-1,y-1,z+1)*aoFactor;
-						f = isBlocked(x+1,y-1,z+1)*aoFactor;
-						g = isBlocked(x+1,y+1,z+1)*aoFactor;
-						h = isBlocked(x-1,y+1,z+1)*aoFactor;
+						e = IS_BLOCKED(x-1,y-1,z+1)*aoFactor;
+						f = IS_BLOCKED(x+1,y-1,z+1)*aoFactor;
+						g = IS_BLOCKED(x+1,y+1,z+1)*aoFactor;
+						h = IS_BLOCKED(x-1,y+1,z+1)*aoFactor;
 					}
 
-					vertex(&index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
-					vertex(&index, x - 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-d-h));
+					VERTEX(index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y + 0.5f, z + 0.5f, u1,v2, l*(1.0f-a-d-h));
 
-					vertex(&index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v1, l*(1.0f-b-c-f));
-					vertex(&index, x + 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y - 0.5f, z + 0.5f, u1,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y - 0.5f, z + 0.5f, u2,v1, l*(1.0f-b-c-f));
+					VERTEX(index, x + 0.5f, y + 0.5f, z + 0.5f, u2,v2, l*(1.0f-a-b-g));
 				}
-				if (!isBlocked(x,y,z-1)){
+				if (!IS_BLOCKED(x,y,z-1)){
+					std::tuple<float, float, float, float>(u1, v1, u2, v2) = calculateUV(block->textures.back);
 					l = 0.8f;
-					auto [u1, v1, u2, v2] = calculateUV(block->textures.back);
 
 					if (ambientOcclusion){
-						a = isBlocked(x,  y+1,z-1)*aoFactor;
-						b = isBlocked(x+1,y,  z-1)*aoFactor;
-						c = isBlocked(x,  y-1,z-1)*aoFactor;
-						d = isBlocked(x-1,y,  z-1)*aoFactor;
+						a = IS_BLOCKED(x,  y+1,z-1)*aoFactor;
+						b = IS_BLOCKED(x+1,y,  z-1)*aoFactor;
+						c = IS_BLOCKED(x,  y-1,z-1)*aoFactor;
+						d = IS_BLOCKED(x-1,y,  z-1)*aoFactor;
 
-						e = isBlocked(x-1,y-1,z-1)*aoFactor;
-						f = isBlocked(x+1,y-1,z-1)*aoFactor;
-						g = isBlocked(x+1,y+1,z-1)*aoFactor;
-						h = isBlocked(x-1,y+1,z-1)*aoFactor;
+						e = IS_BLOCKED(x-1,y-1,z-1)*aoFactor;
+						f = IS_BLOCKED(x+1,y-1,z-1)*aoFactor;
+						g = IS_BLOCKED(x+1,y+1,z-1)*aoFactor;
+						h = IS_BLOCKED(x-1,y+1,z-1)*aoFactor;
 					}
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v2, l*(1.0f-a-d-h));
-					vertex(&index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x - 0.5f, y + 0.5f, z - 0.5f, u2,v2, l*(1.0f-a-d-h));
+					VERTEX(index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-a-b-g));
 
-					vertex(&index, x - 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
-					vertex(&index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-a-b-g));
-					vertex(&index, x + 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-b-c-f));
+					VERTEX(index, x - 0.5f, y - 0.5f, z - 0.5f, u2,v1, l*(1.0f-c-d-e));
+					VERTEX(index, x + 0.5f, y + 0.5f, z - 0.5f, u1,v2, l*(1.0f-a-b-g));
+					VERTEX(index, x + 0.5f, y - 0.5f, z - 0.5f, u1,v1, l*(1.0f-b-c-f));
 				}
 			}
 		}
 	}
-
-    return new Mesh(buffer, index / vertex_SIZE, chunk_attrs);
-}
-
-int BlockRenderer::cdiv(int x, int a)
-{
-    if (x < 0)
-		return (x / a) - 1;
-	return x / a;
-}
-
-int BlockRenderer::localNeg(int x, int size)
-{
-    if (x < 0)
-		return size + x;
-	return x;
-}
-
-int BlockRenderer::local(int x, int size)
-{
-    if (x >= size)
-		return x - size;
-	return localNeg(x, size);
-}
-
-Chunk *BlockRenderer::getChunk(int x, int y, int z)
-{
-	if (!currentChunks)
-		return nullptr;
-    int cx = cdiv(x, CHUNK_W);
-    int cy = cdiv(y, CHUNK_H);
-    int cz = cdiv(z, CHUNK_D);
-    
-    int index = ((cy + 1) * 3 + (cz + 1)) * 3 + (cx + 1);
-    return (Chunk*)currentChunks[index]; 
-}
-
-bool BlockRenderer::isChunk(int x, int y, int z)
-{
-    return getChunk(x, y, z) != nullptr;
-}
-
-blocks::Block* BlockRenderer::block(int x, int y, int z)
-{
-    return blocks::get(getChunk(x, y, z)->blocksIds[local(y, CHUNK_H) * CHUNK_D + local(z, CHUNK_D) * CHUNK_W + local(x, CHUNK_W)]);
-}
-
-bool BlockRenderer::isBlocked(int x, int y, int z)
-{
-    Chunk* chunk = getChunk(x, y, z);
-	if (!chunk)
-		return true;
-	if (block(x, y, z)->id != 0)
-		return true;
-	return false;
-}
-
-void BlockRenderer::vertex(size_t* index, float x, float y, float z, float u, float v, float l)
-{
-	buffer[*index+0] = x;
-	buffer[*index+1] = y;
-	buffer[*index+2] = z;
-	buffer[*index+3] = u;
-	buffer[*index+4] = v;
-	buffer[*index+5] = l;
-	*index += vertex_SIZE;
+	return new Mesh(buffer, index / VERTEX_SIZE, chunk_attrs);
 }
--- src/graphics/BlockRenderer.h
+++ src/graphics/BlockRenderer.h
@@ -7,26 +7,26 @@
 class BlockRenderer {
     float* buffer;
     size_t capacity;
-    const Chunk** currentChunks;
+    Chunk** currentChunks;
 
     public:
         BlockRenderer(size_t capacity);
         ~BlockRenderer();
 
-        std::tuple<int, int, int, int> calculateUV(resources::ResourceLocation *location);
+        std::tuple<float, float, float, float> calculateUV(resources::ResourceLocation *location);
 
         Mesh* render(Chunk* chunk, const Chunk** chunks, bool ambientOcclusion = true);
 
-    private:
-        int cdiv(int x, int a);
-        int localNeg(int x, int size);
-        int local(int x, int size);
-        Chunk* getChunk(int x, int y, int z);
-        bool isChunk(int x, int y, int z);
+    // private:
+        // int cdiv(int x, int a);
+        // int localNeg(int x, int size);
+        // int local(int x, int size);
+        // const Chunk* getChunk(int x, int y, int z, const Chunk** chunks);
+        // bool isChunk(int x, int y, int z, const Chunk** chunks);
 
-        blocks::Block* block(int x, int y, int z);        
-        bool isBlocked(int x, int y, int z);
-        void vertex(size_t* index, float x, float y, float z, float u, float v, float l);
+        // blocks::Block* block(int x, int y, int z, const Chunk** chunks);        
+        // bool isBlocked(int x, int y, int z, const Chunk** chunks);
+        // void vertex(size_t* index, float x, float y, float z, float u, float v, float l);
 
 
 };
