cdbb4e4d075e8471571075d360b407bb81d90092
Malinnik <ratanov.maxim@gmail.com>
2026-03-19 02:32:42 +0500

    feat: slightly bigger world in Y and less in X,Z

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -21,7 +21,7 @@ World::World()
     crosshair = new Crosshair();
 
 
-    chunks = new Chunks(16*4,1,16*4, 0,0,0);
+    chunks = new Chunks(16*2,8,16*2, 0,0,0);
     // chunks = new Chunks(5,3,5);
 
     inputLoop = new InputLoop(camera, chunks);
